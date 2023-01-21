4b44120a4eb2f69aed594cc9abd7c48df727064e
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-01-22 00:31:07 +0100

    add basic rendering

--- parapyro/rendering/defaults.py
+++ parapyro/rendering/defaults.py
@@ -1,7 +1,15 @@
+from parapyro.utils.compiler_utils import set_in_brackets, preempt_slash
+
 class DefaultStyle():
 
     def __init__(self) -> None:
         pass
+
+    def _generate_frontmatter(self):
+
+        return f"""
+{preempt_slash("usecolortheme")}{set_in_brackets("dove")}
+"""
 
 class DefaultTemplate():
 
