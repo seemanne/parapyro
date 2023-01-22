204693e6af57935f08d5fb0b104d4d70d772e995
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-01-22 12:31:02 +0100

    add ImageObject

--- parapyro/slides/object.py
+++ parapyro/slides/object.py
@@ -1,5 +1,46 @@
-class Object():
+from parapyro.utils.compiler_utils import preempt_slash, stack_list_to_lines, begin, end, to_latex_bool, set_in_brackets
+
+class BaseObject():
     
     def __init__(self, name) -> None:
-        
+
         self.name = name
+
+    def compile(self):
+
+        raise NotImplementedError(f"Object does not implement compile()")
+
+    def get_name(self):
+
+        return self.name
+
+class ImageObject():
+
+    def __init__(self, name, filepath) -> None:
+        
+        self.filepath = filepath
+
+        super.__init__(name)
+
+    def _figure_from_file(self, **kwargs):
+        
+        box_params = {
+            "width" : 16,
+            "height" : 9,
+            "keepaspectratio" : True
+        }
+        box_params.update(kwargs)
+        output = ""
+        lines_list = [
+            f"{begin('figure')}",
+            f"{preempt_slash('centering')}",
+            f"{preempt_slash('includegraphics')}[width={str(box_params['width'])}cm, height={str(box_params['height'])}cm, keepaspectratio={to_latex_bool(box_params['keepaspectratio'])}]{set_in_brackets(self.filepath)}",
+            f"{end('figure')}"
+        ]
+        output += stack_list_to_lines(lines_list)
+
+        return output
+
+    def compile(self, **kwargs):
+
+        return self._figure_from_file(**kwargs)
