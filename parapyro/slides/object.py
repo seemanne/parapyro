2d7d42e46770d2b6cdc544a9fa49951938a26616
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-01-22 14:57:12 +0100

    add imageobject_from_pyplot

--- parapyro/slides/object.py
+++ parapyro/slides/object.py
@@ -1,4 +1,6 @@
 from parapyro.utils.compiler_utils import preempt_slash, stack_list_to_lines, begin, end, to_latex_bool, set_in_brackets
+from parapyro import PROJECT_HOME
+from matplotlib.figure import Figure
 
 class BaseObject():
     
@@ -44,3 +46,9 @@ class ImageObject():
     def compile(self, **kwargs):
 
         return self._figure_from_file(**kwargs)
+
+
+def imageobject_from_pyplot(fig: Figure, name):
+
+    fig.savefig(PROJECT_HOME / f"compiled_files/raw/{name}.png")
+    return ImageObject(name, f"compiled_files/raw/{name}.png")
